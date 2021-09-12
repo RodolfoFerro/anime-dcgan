85a2e3a21e6ce40ad863ac1ca2ae7904d02868db
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2021-09-12 10:56:19 -0500

    Initial commit. :fire:

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+from gan import build_dcgan
+from gan import generate_faces
+
+
+dcgan = build_dcgan()
+dcgan.generator.load_weights('models/generator_weights.h5')
+dcgan.discriminator.load_weights('models/discriminator_weights.h5')
+
+
+st.markdown(
+    """
+    # Crea tu waifu
+    
+    **Modelos generativos para la creación de personajes de anime**
+
+    Con esta sencilla aplicación web podrás crear tu waifu con un modelo
+    generativo.
+
+    El modelo generativo se ha entrenado utilizando el conjunto de datos 
+    de Kaggle: [https://www.kaggle.com/soumikrakshit/anime-faces]
+    (https://www.kaggle.com/soumikrakshit/anime-faces)
+
+    Para generar un conjunto de caras, sólo pulsa el siguiente botón:
+    """
+)
+
+if st.button("Crear waifu"):
+    fig = generate_faces()
+    st.pyplot(fig)
+    st.write('Imagen generada por una DCGAN.')
---
+++ gan.py
@@ -0,0 +1,233 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Reshape, Dropout, Dense 
+from tensorflow.keras.layers import Flatten, BatchNormalization
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import LeakyReLU , ReLU, PReLU
+from tensorflow.keras.layers import Conv2D ,Conv2DTranspose
+from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras.optimizers import Adam
+
+
+def build_generator(seed_size):
+    """
+    Builds the generator model
+    
+    Parameters:
+        seed_size: size of the random vector fed into the generator
+    
+    Returns:
+        model: keras model representing the generator
+    """
+    model = Sequential()
+  
+    # Block - 1
+    model.add(Dense(4*4*1024,kernel_initializer=init,input_dim=seed_size))
+    model.add(BatchNormalization())
+    model.add(ReLU())
+    model.add(Reshape((4,4,1024))) # Resulting shape = (4,4,1024) 
+
+    # Block - 2
+    model.add(Conv2DTranspose(512,kernel_size=5,strides=2,padding='same',use_bias=False,kernel_initializer=init))
+    model.add(BatchNormalization())
+    model.add(ReLU())  # Resulting shape = (8,8,512)
+
+    # Block - 3
+    model.add(Conv2DTranspose(256,kernel_size=5,strides=2,padding='same',use_bias=False,kernel_initializer=init))
+    model.add(BatchNormalization())
+    model.add(ReLU()) # Resulting shape = (16,16,256)
+
+    # Block - 4
+    model.add(Conv2DTranspose(128,kernel_size=3,strides=2,padding='same',use_bias=False,kernel_initializer=init))
+    model.add(BatchNormalization())
+    model.add(ReLU()) # Resulting shape = (32,32,128)
+
+    # Block - 5
+    model.add(Conv2DTranspose(3,kernel_size=3,strides=2,padding='same',use_bias=False,kernel_initializer=init))
+    model.add(Activation('tanh')) # Resulting shape = (64,64,3)
+
+    return model
+
+
+def build_discriminator(image_length,image_channels):
+    
+    """
+    Builds the generator model
+    
+    Parameters:
+        image_length: length of a side of the square image
+        image_channels: number of channels in the image
+    
+    Returns:
+        model: keras model representing the discriminator
+    """
+    
+    model = Sequential()
+  
+    # Block - 1
+    model.add(Conv2D(64,kernel_size=3,strides=2,padding='same',use_bias=False,input_shape=(image_length,image_length,image_channels),kernel_initializer=init))
+    model.add(LeakyReLU(alpha=0.2)) # Resulting shape = (32,32,64)
+
+    # Block - 2
+    model.add(Conv2D(128,kernel_size=3,strides=2,padding='same',use_bias=False,kernel_initializer=init))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.2)) # Resulting shape = (16,16,128)
+
+    # Block - 3
+    model.add(Conv2D(256,kernel_size=5,strides=2,padding='same',use_bias=False,kernel_initializer=init))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.2)) # Resulting shape = (8,8,256)
+
+    # Block - 4
+    model.add(Conv2D(512,kernel_size=5,strides=2,padding='same',use_bias=False,kernel_initializer=init))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.2)) # Resulting shape = (4,4,512)
+
+    # Block - 5
+    model.add(Conv2D(1,kernel_size=4,strides=1,padding='valid',use_bias=False,kernel_initializer=init))
+    model.add(Flatten())
+    model.add(Activation('sigmoid'))
+
+    return model
+
+
+cross_entropy = tf.keras.losses.BinaryCrossentropy()
+
+
+class DCGAN(tf.keras.Model):
+    """Subclass of the keras.Model class to define custom training step and loss functions"""
+    
+    
+    def __init__(self,seed_size,image_length,image_channels,**kwargs):
+        """
+        Parameters:
+            seed_size: size of the random vector for the generator
+            image_length: length of a side of the square image
+            image_channels: number of channels in the image
+        """
+        super(DCGAN,self).__init__(**kwargs)
+
+        self.generator = build_generator(seed_size)
+        self.discriminator = build_discriminator(image_length,image_channels)
+        self.seed_size = seed_size
+        
+        
+   # Train generator to give realistic images and penalize it for giving images being classified as fake by the discriminator     
+    def generator_loss(self,fake_output):
+        """
+        Parameters:
+            fake_output: Tensor containing the respective discriminator's predictions for the batch of images produced
+            by generator (fake iamges).
+            
+        Returns:
+            cross entropy loss between labels for real images (1's) and the discriminator's estimate 
+        """
+        
+        # The objective is to penalize the generator whenever it produces images which the discriminator classifies as 'fake'
+        return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+       
+        # smooth parameter is used to induce one sided label smoothing. It can be tuned accordingly
+    def discriminator_loss(self,real_output, fake_output,smooth=0.1):
+        """
+        Parameters: 
+            real_output: Tensor containing the respective discriminator's predictions for the batch of images taken from
+                        the dataset (real images).
+            fake_output: Tensor containing the respective discriminator's predictions for the batch of images produced
+                        by generator (fake images).
+        
+        Returns:
+            total_loss: Loss of the discriminator for misclassifying images
+        """
+        # label for real image is (1-smooth)
+        real_loss = cross_entropy(tf.ones_like(real_output)*(1-smooth), real_output) 
+        fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+        total_loss = real_loss + fake_loss
+        return total_loss
+    
+    def compile(self,generator_optimizer,discriminator_optimizer):
+        """
+        configures model for training by adding optimizers
+        
+        Parameters: 
+            generator_optimizer: keras optimizer to be used to train generator.
+            discriminator_optimizer: keras optimizer to be used to train discriminator.
+        """
+        super(DCGAN, self).compile()
+        self.generator_optimizer = generator_optimizer
+        self.discriminator_optimizer = discriminator_optimizer
+        
+    @tf.function    
+    def train_step(self,data):
+        
+        """
+        Takes in training data and does a forward pass
+        
+        Parameters:
+            data: a batch from the training data.
+        Returns:
+            gen_loss: loss associated with the generator.
+            disc_loss: loss associated with the discriminator.
+            
+        """
+            
+        batch_size = tf.shape(data)[0]
+        
+        # feed a random input to generator
+        seed = tf.random.normal(shape=(batch_size,self.seed_size))
+        
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            
+            # generate image using generator
+            generated_image = self.generator(seed, training = True)
+        
+            # discriminator's prediction for real image
+            real_output = self.discriminator(data,training = True)
+            
+            # discriminator's estimate for fake image
+            fake_output = self.discriminator(generated_image,training = True) 
+            
+            # compute loss
+            gen_loss = self.generator_loss(fake_output)
+            disc_loss = self.discriminator_loss(real_output,fake_output)
+
+            # optimize generator first
+            generator_grad = gen_tape.gradient(gen_loss,self.generator.trainable_variables)
+            discriminator_grad = disc_tape.gradient(disc_loss,self.discriminator.trainable_variables)
+
+            # optimize discriminator after generator
+            self.generator_optimizer.apply_gradients(zip(generator_grad,self.generator.trainable_variables))
+            self.discriminator_optimizer.apply_gradients(zip(discriminator_grad,self.discriminator.trainable_variables))
+        
+        return {
+            "generator loss": gen_loss,
+            "discriminator_loss": disc_loss
+        }
+
+
+def build_dcgan():
+    """
+    Builds the DCGAN model
+    """
+    generator_optimizer = Adam(learning_rate=0.0002,beta_1=0.5)
+    discriminator_optimizer = Adam(learning_rate=0.0002,beta_1=0.5)
+
+    dcgan = DCGAN(128, 64, 3)
+    dcgan.compile(generator_optimizer, discriminator_optimizer)
+    
+    return dcgan
+
+
+def generate_faces(dcgan, seed_size=128):
+    """Generates random Anime faces"""
+    
+    # generate 64 images by giving 64 inputs
+    noise = tf.random.normal([64, seed_size])
+    generated_images = dcgan.generator(noise)
+
+    fig = plt.figure(figsize=(12, 12))
+    for i in range(generated_images.shape[0]):
+        plt.subplot(8, 8, i + 1)
+        plt.imshow((generated_images[i, :, :, :] * 0.5 + 0.5))
+        plt.axis('off')
+    
+    return fig
